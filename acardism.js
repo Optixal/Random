55dcb7ccd4f0ce27590bef97c0cbe250839fe3d2
Optixal <optixal@live.com.sg>
2017-05-15 11:05:33 +0800

    init

---
+++ acardism.js
@@ -0,0 +1,6 @@
+// http://www.slamjamsocialism.com/arcad-ism/
+backup = Runnerism.prototype.gameOverIsm
+Runnerism.prototype.gameOverIsm = function(){}
+Runnerism.instance_.setSpeedIsm(100)
+Runnerism.instance_.setSpeedIsm(1)
+Runnerism.prototype.gameOverIsm = backup
